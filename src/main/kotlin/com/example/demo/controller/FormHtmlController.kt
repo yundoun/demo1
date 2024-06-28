25f581eb9c68e59e4fbba824159adcbefa9cc165
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2024-06-28 15:25:54 +0900

    data class 변수를 html로 복사

--- src/main/kotlin/com/example/demo/controller/FormHtmlController.kt
+++ src/main/kotlin/com/example/demo/controller/FormHtmlController.kt
@@ -1,14 +1,32 @@
 package com.example.demo.controller
 
+import com.example.demo.domain.User
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 
 @Controller
 class FormHtmlController {
     @RequestMapping(
-        value = ["/form"],
+        value = ["/form", "/form/{userid}"],
         method = [RequestMethod.GET]
     )
-    fun index() : String = "form/index"
+    fun index(
+        @PathVariable("userid")userid:Long?, // PathVariable : URL 경로에 변수를 넣어주는 어노테이션
+        model : Model
+    ) : String {
+
+        val user = User(null, null, null)
+
+        if (userid != null) {
+            user.name = "yun do un"
+            user.email = "ehdns1133@naver.com"
+            user.password = "sexyman"
+        }
+
+        model.addAttribute("user", user)
+        return "form/index"
+    }
 }
---
+++ src/main/kotlin/com/example/demo/domain/User.kt
@@ -0,0 +1,7 @@
+package com.example.demo.domain
+
+data class User(
+    var name : String? = null,
+    var email : String? = null,
+    var password : String? = null
+)
